2844b3a5420d2b4eba1cf6216cf22074d529dea4
KonradLatus <konrad.10@vp.pl>
2022-09-10 21:38:34 +0200

    Fourth task done

---
+++ 2.8 Basic operators, maths/Fix the addition.js
@@ -0,0 +1,4 @@
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(+a + +b);  // We need to convert a and b in order to sum them, otherwise the binary plus will add them as strings
